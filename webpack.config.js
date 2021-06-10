7394842b68f7323755bdf7d90697603e2dc40e6f
jypara-git <zhyparaabdieva@gmail.com>
2021-06-10 16:33:26 -0700

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const webpack  = require("webpack");
+
+module.exports = { 
+    // entry point is the root of the bundle and beginning of the dependency graph
+    entry: './assets/js/script.js',
+    // output that bundled code to a folder that we specify
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.bundle.js'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+    ],
+    // the final piece of our basic setup will provide the mode in which we want webpack to return
+    mode: 'development'
+};
