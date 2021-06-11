1409b1364e43887cd6a71954d43fd8d4c8b95554
jypara-git <zhyparaabdieva@gmail.com>
2021-06-10 17:38:26 -0700

    edit package versions

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './assets/js/script.js',
     // output that bundled code to a folder that we specify
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.join(__dirname + '/dist'),
         filename: 'main.bundle.js'
     },
     plugins: [
