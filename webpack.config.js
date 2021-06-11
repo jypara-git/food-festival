b262fc8076fcf5d07f5ee500a34f7f423ab4a29f
jypara-git <zhyparaabdieva@gmail.com>
2021-06-10 23:13:07 -0700

    add multiple points to webpack's configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,28 @@
+const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
-const webpack  = require("webpack");
 
 module.exports = { 
     // entry point is the root of the bundle and beginning of the dependency graph
-    entry: './assets/js/script.js',
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     // output that bundled code to a folder that we specify
     output: {
-        path: path.join(__dirname + '/dist'),
-        filename: 'main.bundle.js'
+        path: __dirname + '/dist',
+        filename: '[name].bundle.js'
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
     ],
     // the final piece of our basic setup will provide the mode in which we want webpack to return
     mode: 'development'
